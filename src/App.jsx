4a711825726513641e8ec3b6e5eadb4665094bc4
Bahtiyar96 <bahtiyartalantbekov@gmail.com>
2023-12-03 15:47:20 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getData } from "./api";
+import dayjs from "dayjs";
+import { getCalendar } from "./utils/calendar";
+import { Cell } from "./components/cell";
+
+function App() {
+  const [data, setData] = useState();
+  const [weeks, setWeeks] = useState(55);
+  const [date, setDate] = useState(dayjs().format("YYYY-MM-DD"));
+
+  useEffect(() => {
+    getData().then((data) => {
+      setData(getCalendar(weeks, data, date));
+    });
+  }, [date, weeks]);
+
+  return (
+    <>
+      <div className="header">
+        <select defaultValue={'50'} onChange={(e) => setWeeks(e.target.value)}>
+          <option value="25">25</option>
+          <option value="50">50</option>
+          <option value="70">70</option>
+        </select>
+        <input
+          value={date}
+          onChange={(e) => {
+            setDate(e.target.value);
+          }}
+          type="date"
+        />
+      </div>
+      {data?.length !== 0 ? (
+        <>
+          <ul className="list">
+            {["Пр", 2, "Ср", 4, "Пт", 6, 7].map((day) => (
+              <span className="days" key={day + "day"}>
+                {typeof day == "string" && day}
+              </span>
+            ))}
+            {data?.map((item) => (
+              <Cell key={item.date + Math.random()} {...item} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        "неверный формат даты"
+      )}
+      <div className="footer">
+        <span>меньше</span>{" "}
+        {["#EDEDED", "#ACD5F2", "#7FA8C9", "#527BA0", "#254E77"].map(
+          (color) => (
+            <div key={color} style={{ background: color }}></div>
+          )
+        )}
+        <span>больше</span>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/api/index.js
@@ -0,0 +1,5 @@
+export const getData = async () => {
+   const res = await fetch('https://dpg.gg/test/calendar.json')
+ 
+   return await res.json()
+ }
